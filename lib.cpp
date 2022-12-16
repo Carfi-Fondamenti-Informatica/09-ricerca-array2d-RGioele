3eab5408d609efc95dc44bb4115478b1f1d25fb8
RGioele <116067942+RGioele@users.noreply.github.com>
2022-12-16 20:36:33 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
 
+#include "lib.h"
+#include <cstring>
+
+bool research(char list [10][20], char name [1][20], int &pos){
+
+    for(int i=0; i<10; i++){
+        if (strcmp (list [i], name [0]) == 0){
+            pos =i;
+            return true;
+        }
+    }
+    return false;
+}
